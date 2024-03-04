6643e701ff0be5cc92b191f65843bf7041bd9287
eugene <qihao.eugenewong@gmail.com>
2024-03-05 00:15:25 +1300

    File

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+from tkinter import *
+from tkinter import filedialog, colorchooser, font
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+def change_color():
+    pass
